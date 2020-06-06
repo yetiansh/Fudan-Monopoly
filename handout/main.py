ab76c9642ff3d767a30a2723886237fb6c8cedab
fields1631 <45711207+fields1631@users.noreply.github.com>
2020-06-06 21:41:47 +0800

    Document structure
    Construct  document structure

---
+++ handout/main.py
@@ -0,0 +1,22 @@
+import pickle
+import dill
+
+from .games import *
+
+
+class Game:
+    def __init__(self):
+        pass
+
+
+class Board:
+    def __init__(self):
+        pass
+
+
+def main(parser):
+    pass
+
+
+def gui(parser):
+    pass
---
+++ source.py
@@ -0,0 +1,12 @@
+import argparse
+
+from handout import *
+
+parser = argparse.ArgumentParser()
+
+#   Add command line options here
+
+parser = parser.parse_args()
+
+if __name__ == '__main__':
+    main(parser)
