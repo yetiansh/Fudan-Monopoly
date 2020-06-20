a0f49df21915423bec0806f2d3a63b5973087ec3
spring520 <66520645+spring520@users.noreply.github.com>
2020-06-20 11:41:27 +0800

    Update makeContents.py

--- makeContents.py
+++ makeContents.py
@@ -5,18 +5,18 @@ from handout.classes import *
 
 grades = ['大一', '大二', '大三', '大四']
 sites = [Site(name="第三教学楼", text="听课", effect=[0, -1, 0, 1], isCompulsory=False),
-         Site(name="第二教学楼", text="听课", effect=[0, -1, 0, 1], isCompulsory=False),
+         Site(name="第二教学楼", text="听课，优越的地理位置（临近本超、全家）保证了课间总有充足的食物补给。", effect=[0, -1, 0, 1], isCompulsory=False),
          Site(name="光华楼小圆桌", text="自习", effect=[0, -1, 0, 1], isCompulsory=False),
-         Site(name="第五教学楼", text="自习", effect=[0, -1, 0, 1], isCompulsory=False),
+         Site(name="第五教学楼", text="在五教自习最快乐的事，就是带有滑轮的椅子，可以自由滑动，坐着很舒适。", effect=[0, -1, 0, 1], isCompulsory=False),
          Site(name="第六教学楼", text="自习", effect=[0, -1, 0, 1], isCompulsory=False),
          Site(name="恒隆物理楼", text="做实验。我的电压表终于接完了，到通电的回合了！测试！咦，这股焦香是……", effect=[0, -1, 0, 1], isCompulsory=False),
          Site(name="光华楼东侧会议室", text="听讲座", effect=[0, -1, 0, 1], isCompulsory=False),
-         Site(name="书呆子", text="买书", effect=[-1, 0, 0, 1], isCompulsory=False),
-         Site(name="理科图书馆", text="自习", effect=[0, -1, 0, 1], isCompulsory=False),
+         Site(name="书呆子", text="买书，这么便宜的教材和小说，买不了吃亏买不了上当，买一本赚一本啊。", effect=[-1, 0, 0, 1], isCompulsory=False),
+         Site(name="理科图书馆", text="理科图书馆，晚上十点敲响赶人的钟声，一批人会走向三教，而我则会走回宿舍", effect=[0, -1, 0, 1], isCompulsory=False),
          Site(name="星空咖啡厅", text="消费", effect=[-1, 0, 1, 0], isCompulsory=True),
          Site(name="南区操场", text="锻炼", effect=[0, -1, 1, 0], isCompulsory=False),
          Site(name="管院足球场", text="有的场地踢足球费鞋，但这一片就有些特别了，费洗衣机。", effect=[0, -1, 1, 0], isCompulsory=False),
-         Site(name="本部篮球场", text="锻炼", effect=[0, -1, 1, 0], isCompulsory=False),
+         Site(name="本部篮球场", text="在复旦之光打过球的场地上打球，我=复旦之光？", effect=[0, -1, 1, 0], isCompulsory=False),
          Site(name="北区体育馆", text="来打排球。但这二楼排球场，男子2.43的网，跳起来手也伸不过网，害。", effect=[0, -1, 1, 0], isCompulsory=False),
          Site(name="同济游泳馆", text="游泳", effect=[0, -1, 1, 0], isCompulsory=False),
          Site(name="宿舍", text="休息", effect=[0, -1, 1, 0], isCompulsory=False),
@@ -29,7 +29,7 @@ sites = [Site(name="第三教学楼", text="听课", effect=[0, -1, 0, 1], isCom
          ]
 
 chances = [{"name": "期中退课", "text": "太菜了，期中退的课，选课犯的罪。-130*n", "effect": [-1, 0, 0, 0], "isCompulsory": True},
-           {"name": "阿康的诱惑", "text": "冲动消费", "effect": [-1, 0, 0, 0], "isCompulsory": True},
+           {"name": "阿康的诱惑", "text": "上课归来，饥肠辘辘，远处飘来烧烤的香气。", "effect": [-1, 0, 0, 0], "isCompulsory": True},
            {"name": "靠一点点续命", "text": "乌龙奶茶加波霸，去冰，三分甜。（私货推荐）", "effect": [-1, 0, 0, 0], "isCompulsory": True}
            {"name": "一卡通丢失", "text": "需要补办一卡通，复旦信息办收入+20。每次都在手掌心感受着新鲜出炉的一卡通特有的温热时，\
            收到了一封邮件'同学，我捡到了你的一卡通……'", "effect": [-1, 0, 0, 0], "isCompulsory": True},
